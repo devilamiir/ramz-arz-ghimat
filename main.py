1c1c93fde9b0d395353286ea92aa0b68c640369a
devil_gh <79138037+devilamiir@users.noreply.github.com>
2025-12-13 01:17:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -64,5 +64,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=port)
